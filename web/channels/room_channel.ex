472258c7a7dfbd4ea67e16ec3f1bba74aebdd97b
BuildTools <ktakashi19980515@gmail.com>
2017-03-25 00:05:14 +0900

    Add Message Login,Sent

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,18 +1,20 @@
 defmodule ChatroomWithPhoenix.RoomChannel do
   use Phoenix.Channel
 
-  def join("room:lobby", _message, socket) do
+  def join("rooms:lobby", _message, socket) do
+    send(self(), :after_join)
     {:ok, socket}
   end
-  def join("room:" <> _private_room_id, _params, _socket) do
+  def join("rooms:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
   def handle_in("new_msg", %{"body" => body}, socket) do
+    push socket, "new_msg", %{body: "Enter"}
     broadcast! socket, "new_msg", %{body: body}
-    {:noreply, socket}
+    {:reply, :ok, socket}
   end
-  def handle_out("new_msg", payload, socket) do
-    push socket, "new_msg", payload
+  def handle_info(:after_join, socket) do
+    broadcast_from! socket, "new_msg", %{body: "Login"}
     {:noreply, socket}
   end
 end
