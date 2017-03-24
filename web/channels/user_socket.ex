4aa9141cd7449d9741494aa25ecc7df6d5c3bd3d
BuildTools <ktakashi19980515@gmail.com>
2017-03-24 18:42:26 +0900

    Add Sample Socket Code in Phoenix's page

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChatroomWithPhoenix.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ChatroomWithPhoenix.RoomChannel
+  channel "room:*", ChatroomWithPhoenix.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
