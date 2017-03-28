96667980c6bcda3297563cce586772cb92328931
BuildTools <ktakashi19980515@gmail.com>
2017-03-28 22:24:21 +0900

    Add function Send Number of people

--- lib/chatroom_with_phoenix/worker.ex
+++ lib/chatroom_with_phoenix/worker.ex
@@ -1,5 +1,6 @@
 defmodule ChatroomWithPhoenix.Worker do
     def start_link do
         Agent.start_link(fn -> ["room1", "room2"] end, name: Rooms)
+        Agent.start_link(fn -> 0 end, name: Count)
     end
 end
--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -18,18 +18,20 @@ defmodule ChatroomWithPhoenix.RoomChannel do
     {:reply, :ok, socket}
   end
   def handle_info({:after_join, user}, socket) do
+    Agent.update(Count,fn n -> n + 1 end)
     socket = assign(socket, :username, user)
-    push socket, "new_msg", %{user: "SYSTEM", body: "Hello #{user}"}
-    broadcast_from! socket, "new_msg", %{user: "SYSTEM", body: "#{user} Login"}
+    push socket, "new_msg", %{user: "SYSTEM", body: "Hello #{user} #{Agent.get(Count, fn n -> n end)}"}
+    broadcast_from! socket, "new_msg", %{user: "SYSTEM", body: "#{user} Login #{Agent.get(Count, fn n -> n end)}"}
     room_info(socket)
     {:noreply, socket}
   end
   def terminate(reason, socket) do
+    Agent.update(Count,fn n -> n - 1 end)
     case reason do
       {:shutdown, :closed} -> IO.puts "Shutdown"
       _ -> IO.puts "Any"
     end
-    broadcast_from! socket, "new_msg", %{user: "SYSTEM", body: "#{socket.assigns[:username]} Logout"}
+    broadcast_from! socket, "new_msg", %{user: "SYSTEM", body: "#{socket.assigns[:username]} Logout #{Agent.get(Count, fn n -> n end)}"}
     :ok
   end
   def room_info(socket) do
